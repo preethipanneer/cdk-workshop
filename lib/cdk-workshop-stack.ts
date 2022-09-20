406662eecf7e7a000880fee23cf3c66b8b807b3e
Preethi Panneerselvam <preethi.panneerselvam@resmed.com>
2022-09-19 22:58:14 -0600

    Initial commit

---
+++ lib/cdk-workshop-stack.ts
@@ -0,0 +1,13 @@
+import { Duration, Stack, StackProps } from 'aws-cdk-lib';
+import * as sns from 'aws-cdk-lib/aws-sns';
+import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Construct } from 'constructs';
+
+export class CdkWorkshopStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    
+  }
+}
